8472542f30841e4a2f6c9c41384f07eeb0f93d71
satoshiking <samoilovn@mail.ru>
2019-11-08 17:55:34 +0300

    PEP8

--- test_form/models.py
+++ test_form/models.py
@@ -4,14 +4,18 @@ from django.contrib.auth.models import User
 
 class Group(models.Model):
     group_text = models.CharField(max_length=200)
+
     def __str__(self):
         return self.group_text
 
+
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
-    group = models.ForeignKey(Group, on_delete=models.CASCADE, default=1)     
+    group = models.ForeignKey(Group, on_delete=models.CASCADE, default=1)
+
     def __str__(self):
         return self.question_text
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
@@ -21,12 +25,11 @@ class Choice(models.Model):
     def __str__(self):
         return self.choice_text
 
+
 class User_choice(models.Model):
-	
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     choice = models.ForeignKey(Choice, on_delete=models.CASCADE)
     checked = models.BooleanField()
 
     def __str__(self):
         return self.user
-
--- test_form/urls.py
+++ test_form/urls.py
@@ -4,9 +4,8 @@ from . import views
 
 app_name = 'test_form'
 urlpatterns = [
-	path('', views.index, name='index'),
-	path('<int:question_id>/', views.detail, name='detail'),
-	path('<int:question_id>/answer/', views.answer, name='answer'),
-	path('<int:group_id>/results/', views.results, name='results'),
+    path('', views.index, name='index'),
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:question_id>/answer/', views.answer, name='answer'),
+    path('<int:group_id>/results/', views.results, name='results'),
 ]
-
